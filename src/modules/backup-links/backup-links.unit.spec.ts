9dc0426a75dc90750cd61e541c3a1216b55bf5c9
Georgian Stan <geeostan8@gmail.com>
2020-12-28 19:34:16 +0200

    test: unit tests for BackupLinksService

---
+++ src/modules/backup-links/backup-links.unit.spec.ts
@@ -0,0 +1,129 @@
+/**
+ * * Mocking
+ */
+
+// * BackupLinksModel
+const MockedBackupLinksModel: any = {
+  raw: {},
+  save: (): void => null,
+};
+
+jest.mock('@modules/backup-links/model/backup-links.model', () => {
+  return {
+    BackupLinksModel: MockedBackupLinksModel,
+  };
+});
+
+// * AppService
+const MockedAppService: any = {
+  logsFolderPath: 'logs',
+};
+
+jest.mock('@modules/app/app.service', () => {
+  return {
+    AppService: MockedAppService,
+  };
+});
+
+// * UtilsService
+const MockedUtilsService: any = {
+  generateRandomId: (path: string): void => null,
+};
+
+jest.mock('@src/common/util/util.service', () => {
+  return {
+    UtilService: MockedUtilsService,
+  };
+});
+
+/**
+ * * Dependencies
+ */
+import { join } from 'path';
+
+/**
+ * * Inner Dependencies
+ */
+import { UtilService } from '@src/common/util/util.service';
+
+/**
+ * * Types
+ */
+import { BackupLinkStatus } from './@types';
+
+/**
+ * * Test Target
+ */
+import { BackupLinksService } from './backup-links.service';
+
+describe('BackupLinksService - Unit Tests', () => {
+  afterEach(() => {
+    MockedBackupLinksModel.raw = {};
+    jest.resetAllMocks();
+  });
+
+  describe('addBackupLink()', () => {
+    //*
+    it('Should add a new backup-link with the default options', () => {
+      const spySave = jest.spyOn(MockedBackupLinksModel, 'save');
+
+      jest.spyOn(UtilService, 'generateRandomId').mockReturnValue('id');
+
+      //@ts-ignore
+      BackupLinksService.addBackupLink({});
+
+      expect(MockedBackupLinksModel.raw.id).toBeDefined();
+
+      expect(MockedBackupLinksModel.raw.id).toEqual({
+        id: 'id',
+        lastBackupTimestamp: 0,
+        status: BackupLinkStatus.PENDING,
+        logsPath: join(MockedAppService.logsFolderPath, `backup-link-id.log`),
+      });
+
+      expect(spySave).toHaveBeenCalled();
+    });
+  });
+
+  describe('removeBackupLink()', () => {
+    //*
+    it('Should remove backup-link', () => {
+      const spySave = jest.spyOn(MockedBackupLinksModel, 'save');
+
+      MockedBackupLinksModel.raw.id = {};
+
+      expect(MockedBackupLinksModel.raw.id).toBeDefined();
+      BackupLinksService.removeBackupLink('id');
+      expect(MockedBackupLinksModel.raw.id).not.toBeDefined();
+
+      expect(spySave).toHaveBeenCalled();
+    });
+  });
+
+  describe('listBackupLinksByNames()', () => {
+    //*
+    it('Should return the linkName for each backup-link', () => {
+      MockedBackupLinksModel.raw.id = { linkName: 'one' };
+      MockedBackupLinksModel.raw.secondId = { linkName: 'two' };
+
+      expect(BackupLinksService.listBackupLinksByNames()).toEqual([
+        'one',
+        'two',
+      ]);
+    });
+  });
+
+  describe('backupLinksNameToIdMap()', () => {
+    //*
+    it("Should return the id of a backup link based on it's", () => {
+      MockedBackupLinksModel.raw.id = { linkName: 'one' };
+
+      expect(BackupLinksService.backupLinksNameToIdMap('one')).toBe('id');
+    });
+
+    //*
+    it("Should return undefined if there is no backup link with the supplied nameid of a backup link based on it's", () => {
+      expect(BackupLinksService.backupLinksNameToIdMap('one')).toBeUndefined();
+    });
+  });
+});
